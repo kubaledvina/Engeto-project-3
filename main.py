8a6bc4a56a2772e34abc73d0f01f161975d80624
Jakub Ledvina <kubaledvina@gmail.com>
2025-03-16 08:41:36 +0100

    check_game - kontrola jednotlivých polí na desce

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ separator = "=" * 30
 def playing_board(board):
     '''
     definování hrací desky pomocí cyklu
-
+    :param board: herní pole 3x3
     '''
     print(separator)
     for row in board:
@@ -18,7 +18,7 @@ def playing_board(board):
     print("+---+---+---+")
     print(separator)
 """
-testovací výpis
+#testovací výpis
 test = [
         ["0", " ", "x"],
         ["0", " ", "x"],
@@ -26,4 +26,43 @@ test = [
     ]
 
 playing_board(test)
+"""
+
+def check_game(board, player):
+    """
+    kontroluje zda hráč vyhrál hru
+
+    :param board: herní pole 3x3
+    :param player: Symbol pro hráče 'X' nebo 'O'
+    :return: True pokud hráč vyhrál jinak False
+    """
+    #kontrola symbolů v řádků jestli jsou tři stejné symboly v řádku
+    for row in board:
+        if row[0] == row[1] == row[2] == player:
+            return True  #pokud ano hráč vyhrál
+
+    #kontrola symbolů v sloupců jestli jsou tři stejné symboly v řádku
+    for column in range(3):
+        if board[0][column] == board[1][column] == board[2][column] == player:
+            return True #pokud ano hráč vyhrál
+
+    #kontrola diagonály zleva -> do prava dolů
+    if board[0][0] == board[1][1] == board[2][2] == player:
+        return True #pokud ano hráč vyhrál
+
+    #kontrola diagonály zprava -> doleva dolů
+    if board[0][2] == board[1][1] == board[2][0] == player:
+        return True #pokud ano hráč vyhrál
+
+    #pokud žádná podmínka neplatí vrátí False(nevyhrál) hra pokračuje
+    return False
+"""
+#testovací výpis
+test = [
+        ["O", " ", "x"],
+        ["O", "O", "O"],
+        ["O", " ", "x"],
+    ]
+
+print(check_game(test, "O"))
 """
