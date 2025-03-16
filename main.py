79621fd6ca0863bf5c97b6ae52b96a929f68d46f
Jakub Ledvina <kubaledvina@gmail.com>
2025-03-16 09:38:05 +0100

    draw - funkce pro remízu

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ separator = "=" * 30
 def playing_board(board):
     '''
     definování hrací desky pomocí cyklu
-    :param board: herní pole 3x3
+    :param board: herní deska 3x3
     '''
     print(separator)
     for row in board:
@@ -32,16 +32,16 @@ def check_game(board, player):
     """
     kontroluje zda hráč vyhrál hru
 
-    :param board: herní pole 3x3
+    :param board: herní deska 3x3
     :param player: Symbol pro hráče 'X' nebo 'O'
     :return: True pokud hráč vyhrál jinak False
     """
-    #kontrola symbolů v řádků jestli jsou tři stejné symboly v řádku
+    #kontrola symbolů v řádkách jestli jsou tři stejné symboly v řádku
     for row in board:
         if row[0] == row[1] == row[2] == player:
             return True  #pokud ano hráč vyhrál
 
-    #kontrola symbolů v sloupců jestli jsou tři stejné symboly v řádku
+    #kontrola symbolů ve sloupcích jestli jsou tři stejné symboly v řádku
     for column in range(3):
         if board[0][column] == board[1][column] == board[2][column] == player:
             return True #pokud ano hráč vyhrál
@@ -65,4 +65,28 @@ test = [
     ]
 
 print(check_game(test, "O"))
+"""
+
+def draw(board):
+    """
+    Kontrola polí pokud jsou všechna pole obsazená je remíza
+
+    :param board: herní deska 3x3
+    :return: True pokud je remíza jinak False
+    """
+    for row in board:
+        for box in row:
+            if box == " ":
+                return False
+
+    return True
+"""
+#test remízy
+test = [
+    ["X", "O", "X"],
+    ["X", "X", "O"],
+    ["O", "X", "O"]
+]
+
+print(draw(test)) 
 """
